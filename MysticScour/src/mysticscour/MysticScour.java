f6163f62d83628b1ba93fc63c7b9148d06736f9e
mdh44 <mdh44@students.uwf.edu>
2016-11-21 11:32:11 -0600

    Got a main menu to show

--- MysticScour/src/mysticscour/MysticScour.java
+++ MysticScour/src/mysticscour/MysticScour.java
@@ -1,39 +1,40 @@
 package mysticscour;
 
+import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class MysticScour extends Application {
-    
+
+    private Stage primaryStage;
+    private AnchorPane mainPane;
+
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Main menu");
+        showMainMenuPane();
+    }
+
+    private void showMainMenuPane() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MysticScour.class.getResource("view/Main Menu.fxml"));
+            mainPane = (AnchorPane) loader.load();
             
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+            Scene scene = new Scene(mainPane);
+            primaryStage.setScene(scene);         
+            primaryStage.show();
+            
+        } catch (IOException ex) {
+        }
     }
 
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
--- MysticScour/src/mysticscour/view/MainMenuController.java
+++ MysticScour/src/mysticscour/view/MainMenuController.java
@@ -1,19 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mysticscour.view;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author Matthew
- */
 public class MainMenuController implements Initializable {
 
     /**
