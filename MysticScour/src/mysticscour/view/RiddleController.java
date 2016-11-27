663121208993b2c788ed69e816d5557b15da9fcc
mdh44 <mdh44@students.uwf.edu>
2016-11-27 16:40:13 -0600

    update for riddle puzzle

--- MysticScour/src/mysticscour/view/RiddleController.java
+++ MysticScour/src/mysticscour/view/RiddleController.java
@@ -1,15 +1,46 @@
-
 package mysticscour.view;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
 public class RiddleController implements Initializable {
+
+    @FXML
+    private Button submit, item;
+
+    @FXML
+    private Label response;
+
+    @FXML
+    private TextField tf;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
-    }    
-    
+    }
+
+    @FXML
+    private void testAnwser(ActionEvent event) throws IOException {
+        if (event.getSource() == item) {
+            response.setText("Skipped");
+        }
+        if (event.getSource() == submit) {
+            String anwser = tf.getText();
+            if (anwser.equalsIgnoreCase("Potato")) {
+                response.setText("Correct");
+            } else {
+                response.setText("Incorrect");
+            }
+
+        }
+    }
+
 }
