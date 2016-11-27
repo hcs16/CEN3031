212830fa71da42681c9d1c25bb4b188bc863c4ec
mdh44 <mdh44@students.uwf.edu>
2016-11-27 15:27:12 -0600

    replace any files that you have worked on, all lot of these are placeholder

---
+++ MysticScour/src/mysticscour/view/RiddleController.java
@@ -0,0 +1,15 @@
+
+package mysticscour.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class RiddleController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }    
+    
+}
