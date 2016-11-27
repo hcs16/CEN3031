97a51f58e0d076317381623ef4748965ab9140d0
mdh44 <mdh44@students.uwf.edu>
2016-11-27 15:14:45 -0600

    Replace the relevent fxml files with yours

---
+++ mysticscour/src/mysticscour/model/MatchingPuzzle.java
@@ -0,0 +1,9 @@
+package mysticscour.model;
+
+public class MatchingPuzzle {
+    
+    private void test()
+    {
+        System.out.println("YO");
+    }
+}
---
+++ mysticscour/src/mysticscour/view/MatchingController.java
@@ -0,0 +1,14 @@
+
+package mysticscour.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class MatchingController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }    
+    
+}
